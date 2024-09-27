0e43f1d2c2f63e521ec1248a611e56d57728de57
sidchandani <siddharthchandani0013@gmail.com>
2024-09-27 15:39:38 +0530

    first

---
+++ auctions/models.py
@@ -0,0 +1,123 @@
+"""Contains all models used in the app."""
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+class User(AbstractUser):
+    """User model - inherited from Django implementation"""
+    pass
+
+class Auction(models.Model):
+    """Auction model contains all info about one auction:
+    * auction's title
+    * auction's description
+    * who is selling
+    * auction's current price
+    * when auction was publicated
+    * what is auction's category
+    * auction's image URL
+    * is auction closed?
+    """
+
+    # Categories - choices
+    MOTORS = "MOT"
+    FASHINON = "FAS"
+    ELECTRONICS = "ELE"
+    COLLECTIBLES_ARTS = "ART"
+    HOME_GARDES = "HGA"
+    SPORTING_GOODS = "SPO"
+    TOYS = "TOY"
+    BUSSINES_INDUSTRIAL = "BUS"
+    MUSIC = "MUS"
+
+    CATEGORY = [
+        (MOTORS, "Motors"),
+        (FASHINON, "Fashion"),
+        (ELECTRONICS, "Electronics"),
+        (COLLECTIBLES_ARTS, "Collectibles & Art"),
+        (HOME_GARDES, "Home & Garden"),
+        (SPORTING_GOODS, "Sporting Goods"),
+        (TOYS, "Toys"),
+        (BUSSINES_INDUSTRIAL, "Business & Industrial"),
+        (MUSIC, "Music"),
+    ]
+
+    # Model fields
+    # auto: auction_id
+    seller = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=250, blank=False)
+    description = models.TextField(blank=True)
+    current_price = models.DecimalField(max_digits=11, decimal_places=2, default=0.0)
+    category = models.CharField(max_length=3, choices=CATEGORY, default=MOTORS)
+    image_url = models.URLField(blank=True)
+    publication_date = models.DateTimeField(auto_now_add=True)
+    closed = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "auction"
+        verbose_name_plural = "auctions"
+
+    def __str__(self):
+        return f"Auction id: {self.id}, title: {self.title}, seller: {self.seller}"
+
+class Bid(models.Model):
+    """Bid model contains all info about single bid:
+    * price
+    * who bid
+    * when
+    * on what auction
+    """
+
+    # Model fields
+    # auto: bid_id
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    bid_date = models.DateTimeField(auto_now_add=True)
+    bid_price = models.DecimalField(max_digits=11, decimal_places=2)
+
+    class Meta:
+        verbose_name = "bid"
+        verbose_name_plural = "bids"
+
+    def __str__(self):
+        return f"{self.user} bid {self.bid_price} $ on {self.auction}"
+
+class Comment(models.Model):
+    """Comment model contains all info about single comment
+    * content
+    * who posted
+    * when
+    * on what auction
+    """
+
+    # Model fields
+    # auto: comment_id
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField(blank=False)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    class Meta:
+        verbose_name = "comment"
+        verbose_name_plural = "comments"
+
+    def __str__(self):
+        return f"Comment {self.id} on auction {self.auction} made by {self.user}"
+
+class Watchlist(models.Model):
+    """Watchlist model contains all info about object on watchlist
+    * which auction is on watchlist
+    * on whose watchlist this auction is
+    """
+
+    # Model field
+    # auto: watchlist_id
+    auction = models.ForeignKey(Auction, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watchlist")
+
+    class Meta:
+        verbose_name = "watchlist"
+        verbose_name_plural = "watchlists"
+        # Forces to not have auction duplicates for one user
+        unique_together = ["auction", "user"]
+
+    def __str__(self):
+        return f"{self.auction} on user {self.user} watchlist"
